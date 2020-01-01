akerouanton/zbuild

commit 5739d59eaca4ce762ec3cbae9286b57a31532b68

Add `zbuild llbgraph` command

Makefile
@@ -30,6 +30,7 @@ gen-mocks:
 .PHONY: gen-testdata
 gen-testdata:
 	@$(GOTEST) -v ./pkg/llbutils -testdata
+	@$(GOTEST) -v ./pkg/llbgraph -testdata
 	@$(GOTEST) -v ./pkg/defkinds/php -testdata
 	@$(GOTEST) -v ./pkg/defkinds/webserver -testdata
 	@echo "WARNING: Be sure to review regenerated testdata files before committing them."

cmd/zbuild/debug_llb.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/NiR-/zbuild/pkg/builder"
+	"github.com/NiR-/zbuild/pkg/llbutils"
 	"github.com/NiR-/zbuild/pkg/registry"
 	"github.com/moby/buildkit/client/llb"
 	"github.com/sirupsen/logrus"
@@ -14,29 +15,41 @@ var debugFlags = struct {
 	file    string
 	stage   string
 	context string
+	asJSON  bool
 }{}
 
 // @TODO: buildctl should not be required
-const debugDescription = `Output LLB DAG in binary format.
+const debugDescription = `Output LLB DAG in binary or JSON format.
 
 This command alone is not really useful. To have a readable output, you have to
-pipe its output to ` + "`buildctl debug dump-llb`" + `:
+either pipe its output to ` + "`buildctl debug dump-llb`" + `:
 
-	zbuild debug-llb | buildctl debug dump-llb | jq -C . | less -R
+	$ zbuild debug-llb | buildctl debug dump-llb | jq -C . | less -R
+
+Or you can generate JSON dumps like the ones used by most zbuild testcases
+using:
+
+	$ zbuild debug-llb --json
+
+You can also pipe this last command into ` + "`zbuild llbgraph`" + `:
+
+	$ zbuild debug-llb --json | zbuild llbgraph
 `
 
 func newDebugLLBCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:    "debug-llb",
 		Hidden: true,
-		Short:  "Output LLB DAG in binary format.",
+		Short:  "Output LLB DAG in binary or JSON format.",
 		Long:   debugDescription,
 		Run:    HandleDebugLLBCmd,
 	}
 
-	cmd.Flags().StringVarP(&debugFlags.stage, "target", "t", "dev", "Name of the stage to debug")
 	AddFileFlag(cmd, &debugFlags.file)
 	AddContextFlag(cmd, &debugFlags.context)
+
+	cmd.Flags().StringVarP(&debugFlags.stage, "stage", "s", "dev", "Name of the stage to debug")
+	cmd.Flags().BoolVar(&debugFlags.asJSON, "json", false, "Output the LLB DAG in JSON format")
 
 	return cmd
 }
@@ -52,10 +65,23 @@ func HandleDebugLLBCmd(cmd *cobra.Command, args []string) {
 		logrus.Fatalf("%+v", err)
 	}
 
+	if debugFlags.asJSON {
+		// @TODO: merge llbtest into llbutils
+		out, err := llbutils.StateToJSON(state)
+		if err != nil {
+			logrus.Fatalf("%+v", err)
+		}
+
+		if _, err := os.Stdout.Write(out); err != nil {
+			logrus.Fatalf("%+v", err)
+		}
+
+		return
+	}
+
 	out, err := state.Marshal(llb.LinuxAmd64)
 	if err != nil {
 		logrus.Fatalf("%+v", err)
 	}
-
 	llb.WriteTo(out, os.Stdout)
 }

cmd/zbuild/llbgraph.go
@@ -1,0 +1,74 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/NiR-/zbuild/pkg/llbgraph"
+	"github.com/NiR-/zbuild/pkg/llbutils"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var llbgraphFlags = struct {
+	input    string
+	output   string
+	rawAttrs bool
+}{}
+
+func newLLBGraphCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:    "llbgraph",
+		Hidden: true,
+		Short:  "Output LLB DAG in DOT format.",
+		Long: `Output LLB DAG in DOT format.
+
+Transform a JSON dump created with ` + "`zbuild debug-llb`" + `into a DOT digraph
+representation. This can be used to take insights about what build steps are
+generated by zbuild.
+
+Example (feh is used to open up the generated PNG file):
+
+	$ zbuild debug-llb --json | zbuild llbgraph | dot /dev/stdin -o /dev/stdout -T png | feh -
+`,
+		Run: HandleLLBGraphCmd,
+	}
+
+	cmd.Flags().StringVar(&llbgraphFlags.input, "input", "-", "Where the input JSON file is located (use - or left empty to use stdin)")
+	cmd.Flags().StringVar(&llbgraphFlags.output, "output", "", "Where the PNG file should be written.")
+	cmd.Flags().BoolVar(&llbgraphFlags.rawAttrs, "raw", false, "Include raw LLB ops in node labels")
+
+	return cmd
+}
+
+func HandleLLBGraphCmd(cmd *cobra.Command, args []string) {
+	if llbgraphFlags.input == "-" || llbgraphFlags.input == "" {
+		llbgraphFlags.input = "/dev/stdin"
+	}
+	if llbgraphFlags.output == "-" || llbgraphFlags.output == "" {
+		llbgraphFlags.output = "/dev/stdout"
+	}
+
+	rawJSON, err := ioutil.ReadFile(llbgraphFlags.input)
+	if err != nil {
+		logrus.Fatalf("%+v", err)
+	}
+
+	def, err := llbutils.JSONToDefinition(rawJSON)
+	if err != nil {
+		logrus.Fatalf("%+v", err)
+	}
+
+	opts := llbgraph.GraphOpts{
+		RawAttrs: llbgraphFlags.rawAttrs,
+	}
+	g, err := llbgraph.ToDotGraph(def, opts)
+	if err != nil {
+		logrus.Fatalf("%+v", err)
+	}
+
+	rawGraph, err := g.MarshalText()
+	if err != nil {
+		logrus.Fatalf("%+v", err)
+	}
+	ioutil.WriteFile(llbgraphFlags.output, []byte(rawGraph), 0640)
+}

cmd/zbuild/root.go
@@ -25,6 +25,7 @@ func main() {
 
 	zbuildCmd.AddCommand(newUpdateCmd())
 	zbuildCmd.AddCommand(newDebugLLBCmd())
+	zbuildCmd.AddCommand(newLLBGraphCmd())
 
 	if err := zbuildCmd.Execute(); err != nil {
 		logrus.Fatalf("%+v", err)

commit 66173d9450aca9e9df316d4ed34132d96ca10134

Remove -cover option
It looks like -cover option is preventing `go test` to build llbgraph
package, instead emitting following error:

> # github.com/NiR-/zbuild/pkg/llbgraph
> vet: $WORK/b314/llbgraph.cover.go:1:1: expected 'package', found 'EOF'

After bisecting the last commits, it appears this issue was introduced
by: 5739d59 Add `zbuild llbgraph` command.

Makefile
@@ -20,8 +20,9 @@ build:
 
 .PHONY: test
 test:
-	$(GOTEST) -v -cover -coverprofile cover.out ./...
-	go tool cover -o cover.html -html=cover.out
+	# $(GOTEST) -v -cover -coverprofile cover.out ./...
+	$(GOTEST) -v ./...
+	# go tool cover -o cover.html -html=cover.out
 
 .PHONY: gen-mocks
 gen-mocks:
@@ -33,7 +34,7 @@ gen-testdata:
 	@$(GOTEST) -v ./pkg/llbgraph -testdata
 	@$(GOTEST) -v ./pkg/defkinds/php -testdata
 	@$(GOTEST) -v ./pkg/defkinds/webserver -testdata
-	@echo "WARNING: Be sure to review regenerated testdata files before committing them."
+	@echo "WARNING: Be sure to review generated testdata files before committing them."
 
 .PHONY: build-image
 build-image: .validate-image-tag build